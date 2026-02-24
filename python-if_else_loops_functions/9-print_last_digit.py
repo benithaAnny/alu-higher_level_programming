58123bb1b647af25c8dff984816628ffa7a1e919
benithaAnny <b.akuzwe@alustudent.com>
2026-02-24 06:29:48 -0800

    now

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
+def uppercase(str):
+    for letters in str:
+        if ord(letters) >= 97 and ord(letters) <= 122:
+            letters = ord(letters) - 32
+        else:
+            letters = ord(letters)
+        print("{:c}".format(letters), end="")
+    print("")
