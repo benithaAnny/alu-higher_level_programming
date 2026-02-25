2328c5ad07b930995ddefaf3cdaaf5d89c226d1e
benithaAnny <b.akuzwe@alustudent.com>
2026-02-25 05:31:50 -0800

    now

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     import hidden_4
     for item in dir(hidden_4):
         if item[0:2] != "__":
-            print("{:s}".format(item)
+            print("{:s}".format(item))
